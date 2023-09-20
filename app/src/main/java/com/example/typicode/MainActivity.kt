cff32efbb52a9a9c88c04100c6bef0c74283cf0c
Daniel Perez <doperez1997@gmail.com>
2023-09-19 20:40:58 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/typicode/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.typicode
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.typicode.ui.theme.TypicodeExampleTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    var text by mutableStateOf("Hello!")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            // TODO
+        }
+
+        setContent {
+            TypicodeExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text)
+                }
+            }
+        }
+    }
+}
